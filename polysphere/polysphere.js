d505c5c67f909f11ea5bb4db325ad28e1b3df720
zm207 <115565207+zm207@users.noreply.github.com>
2022-11-07 16:41:04 +0000

    Add files via upload

---
+++ polysphere/polysphere.js
@@ -0,0 +1,13 @@
+class PolysphereSolver {
+    h; // height of grid
+    w; // width of grid
+    p; // pieces that can be inserted into the grid
+    constructor(height, width, pieces) {
+
+    }
+
+    solve() {
+        alert("not yet implemented", "");
+    }
+
+}
